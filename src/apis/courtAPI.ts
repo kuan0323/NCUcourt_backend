4f2d293c32d2e6026ffe43dea304a7b38b72ef4f
FuHarrison <fuh.harrison.lex@gmail.com>
2022-12-06 22:47:35 +0800

    APIs of courts - first commit

--- src/apis/courtAPI.ts
+++ src/apis/courtAPI.ts
@@ -1,22 +1,93 @@
-// import * as Koa from 'koa';
-// import database from './database/mongoDatabase';
+import * as Koa from "koa";
+import database from "./database/mongoDatabase";
 
-// export default {
-       // get all courts information from the type(ex. basketball) we clicked
-       // hint: three type of court (basketball、badminton、volleyball)
-//     async getCourts (ctx: Koa.Context) {
-        
-//     }
+export default {
+  // get all courts information from the type(ex. basketball) we clicked
+  // hint: three type of court (basketball、badminton、volleyball)
+  async getCourts(ctx: Koa.Context) {
+    const type_keyword = ctx.query.type_keyword;
+    const collection = await database.getCollection("courts");
+    const courts = await collection
+      .find({ court_type: type_keyword })
+      .toArray();
+    ctx.body = courts;
+  },
+  async createCourts(ctx: Koa.Context) {
+    const court_name = ctx.request.body.court_name;
+    const court_photo = ctx.request.body.court_photo;
+    const court_price = ctx.request.body.court_price;
+    const court_status = ctx.request.body.court_status;
+    const court_type = ctx.request.body.court_type;
 
-//     async createCourts (ctx: Koa.Context) {
-        
-//     }
+    ctx.request.body.createdTime = new Date();
+    const createdTime = ctx.request.body.createdTime;
+    const collection = await database.getCollection("courts");
 
-//     async editCourts (ctx: Koa.Context) {
+    //check if name is used
+    if (
+      (await collection.find({ court_name: court_name }).toArray()).length === 0
+    ) {
+      const result = await collection.insertOne({
+        court_name: court_name,
+        court_photo: court_photo,
+        court_price: court_price,
+        court_status: court_status,
+        court_type: court_type,
+        createdTime: createdTime,
+      });
+      ctx.body = result.ops[0];
+    } else {
+      ctx.body = "Warning: The court name had been used!";
+    }
+  },
 
-//     }
+  async editCourts(ctx: Koa.Context) {
+    const court_photo = ctx.request.body.court_photo;
+    const court_price = ctx.request.body.court_price;
+    const court_status = ctx.request.body.court_status;
+    const court_type = ctx.request.body.court_type;
 
-//     async deleteCourts (ctx: Koa.Context) {
+    const name_keyword = ctx.query.name_keyword;
+    const collection = await database.getCollection("courts");
+    ctx.request.body.lastModified = new Date();
+    const lastModified = ctx.request.body.lastModified;
 
-//     }
-// }
+    //check if the target exist
+    if (
+      (await collection.find({ court_name: name_keyword }).toArray()).length ===
+      0
+    ) {
+      ctx.body = "Warning: Can't find the court!";
+    } else {
+      const courts = await collection.updateOne(
+        { court_name: name_keyword },
+        {
+          $set: {
+            court_photo: court_photo,
+            court_price: court_price,
+            court_status: court_status,
+            court_type: court_type,
+            lastModified: lastModified,
+          },
+        }
+      );
+      ctx.body = await collection.find({ court_name: name_keyword }).toArray();
+    }
+  },
+
+  async deleteCourts(ctx: Koa.Context) {
+    const name_keyword = ctx.query.name_keyword;
+    const collection = await database.getCollection("courts");
+
+    //check if the target exist
+    if (
+      (await collection.find({ court_name: name_keyword }).toArray()).length ===
+      0
+    ) {
+      ctx.body = "Warning: Can't find the court!";
+    } else {
+      const deleting = await collection.deleteOne({ court_name: name_keyword });
+      ctx.body = "The court had been deleted";
+    }
+  },
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,45 +1,45 @@
-import * as Router from 'koa-router';
+import * as Router from "koa-router";
 import reservationAPI from "./apis/reservationAPI";
 import userAPI from "./apis/userAPI";
-// import courtAPI from "./apis/courtAPI";
+import courtAPI from "./apis/courtAPI";
 // import adminAPI from "./apis/adminAPI";
 // import messageAPI from "./apis/messageAPI";
 
 export default (router: Router) => {
-    router.get('/api/users', userAPI.getUsers);
-    router.get('/api/users/:id', userAPI.getUsers);
-    router.post('/api/register', userAPI.register);
+  router.get("/api/users", userAPI.getUsers);
+  router.get("/api/users/:id", userAPI.getUsers);
+  router.post("/api/register", userAPI.register);
 
-    // router.get('/api/loginUsers', userAPI.loginUsers);
-    // router.put('/api/Users', userAPI.editUsers);
-    // router.delete('/api/Users', userAPI.deleteUsers);
+  router.get("/api/loginUsers", userAPI.loginUsers);
+  // router.put('/api/Users', userAPI.editUsers);
+  // router.delete('/api/Users', userAPI.deleteUsers);
 
-    router.get('/api/reservations', reservationAPI.getReservations);
-    // router.post('/api/reservations', reservationAPI.createReservations);
-    // router.put('/api/reservations', reservationAPI.editReservations);
-    // router.delete('/api/reservations', reservationAPI.deleteReservations);
+  router.get("/api/reservations", reservationAPI.getReservations);
+  // router.post('/api/reservations', reservationAPI.createReservations);
+  // router.put('/api/reservations', reservationAPI.editReservations);
+  // router.delete('/api/reservations', reservationAPI.deleteReservations);
 
-    // router.get('/api/courts', courtAPI.getCourts);
-    // router.post('/api/courts', courtAPI.createCourts);
-    // router.put('/api/courts', courtAPI.editCourts);
-    // router.delete('/api/courts', courtAPI.deleteCourts);
+  router.get("/api/courts", courtAPI.getCourts);
+  router.post("/api/courts", courtAPI.createCourts);
+  router.put("/api/courts", courtAPI.editCourts);
+  router.delete("/api/courts", courtAPI.deleteCourts);
 
-    // router.get('/api/admins', adminAPI.getAdmins);
-    // router.post('/api/admins', adminAPI.createAdmins);
-    // router.put('/api/admins', adminAPI.editAdmins);
-    // router.delete('/api/admins', adminAPI.deleteAdmins);
+  // router.get('/api/admins', adminAPI.getAdmins);
+  // router.post('/api/admins', adminAPI.createAdmins);
+  // router.put('/api/admins', adminAPI.editAdmins);
+  // router.delete('/api/admins', adminAPI.deleteAdmins);
 
-    // router.get('/api/messages', messageAPI.getMessages);
-    // router.post('/api/messages', messageAPI.createMessages);
-    // router.put('/api/messages', messageAPI.editMessages);
-    // router.delete('/api/messages', messageAPI.deleteMessages);
+  // router.get('/api/messages', messageAPI.getMessages);
+  // router.post('/api/messages', messageAPI.createMessages);
+  // router.put('/api/messages', messageAPI.editMessages);
+  // router.delete('/api/messages', messageAPI.deleteMessages);
 
-    // post
-    // put
-    // delete
+  // post
+  // put
+  // delete
 
-    // HTTP pass argument
-    // 1. query string
-    // 2. path parameter
-    // 3. post body
-}
+  // HTTP pass argument
+  // 1. query string
+  // 2. path parameter
+  // 3. post body
+};
