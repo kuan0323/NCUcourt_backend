0e46392519ca76f57ab49bb62fa90bc29655442a
kuan0323 <ronni31027@gmail.com>
2022-12-13 16:26:00 +0800

    fix conflict from yo branch

--- src/apis/reservationAPI.ts
+++ src/apis/reservationAPI.ts
@@ -5,10 +5,55 @@ export default {
     // User can view records
     async getReservations (ctx: Koa.Context) {
         ctx.body = {};
-    }
+    },
 
-    // async createReservations (ctx: Koa.Context) {
+    async createReservations (ctx: Koa.Context) {
+        const court = ctx.request.body.court;
+        const student = ctx.request.body.student;
+        const email = ctx.request.body.email;
+        const phone = ctx.request.body.phone;
+        const date = ctx.request.body.date;
+        const time = ctx.request.body.time;
 
+        const collection = await database.getCollection("reservations");
+
+        // let id = await collection.find().count();
+
+        ctx.request.body.createdTime = new Date();
+        const createdTime = ctx.request.body.createdTime;
+        
+        if( 
+            (await collection.find({ 
+                reservation_court_name: court,
+                reservation_date: date,
+                reservation_time: time,
+            }).toArray()).length === 0
+        ){
+        const result = await collection.insertOne({
+            courtName : court, 
+            studentId: student,
+            studentEmail: email, 
+            studentPhone: phone, 
+            createdTime: createdTime,
+            date: date,
+            time: time
+        });
+
+        ctx.body = result.ops[0];
+        }
+    },
+
+    async editReservations (ctx: Koa.Context) {
+        
+        const student = ctx.request.body.student;
+        const email = ctx.request.body.email;
+        const phone = ctx.request.body.phone;
+        const date = ctx.request.body.date;
+        const time = ctx.request.body.time;
+
+        const id_keyword = ctx.request.body.id_keyword;
+
+        const collection = await database.getCollection("reservations");
     // }
 
     // async editReservations (ctx: Koa.Context) {
