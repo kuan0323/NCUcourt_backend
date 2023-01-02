74978e6f248ec3b59dea9b4c799b3079a2fe56fc
楊宗翰 <feveraly@gmail.com>
2023-01-02 18:35:48 +0800

    fix: import name error

--- src/binding.ts
+++ src/binding.ts
@@ -4,7 +4,7 @@ import './adapters/mongo/mongoUserService';
 import './adapters/mongo/mongoCourtService';
 import './adapters/mongo/mongoReservationService';
 import './adapters/mongo/mongoMessageService';
-import './adapters/storage_access/GCPImageStorage';
+import './adapters/storage_access/gcpImageStorage';
 export default () => {
 
 }
